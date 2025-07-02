1979b2e121d799c58d0090eecaa470e035b2f9c4
Robin Lugo Boero <rjlb991102@hotmail.com>
2025-07-01 21:57:14 -0500

    first commit

---
+++ src/main/java/com/robinlb99/legalserviceportal/LegalserviceportalApplication.java
@@ -0,0 +1,13 @@
+package com.robinlb99.legalserviceportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalserviceportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalserviceportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robinlb99/legalserviceportal/LegalserviceportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robinlb99.legalserviceportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalserviceportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
