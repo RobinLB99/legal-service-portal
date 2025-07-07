9edfc9a440dc84cc345e91054e4b33c158488473
Robin Lugo Boero <rjlb991102@hotmail.com>
2025-07-06 23:11:21 -0500

    feat(config): configure entity scanning
    This enables Spring to discover and manage JPA entities located in the `com.robinlb99.legalserviceportal.domain` package, supporting the recently added domain layer.

--- src/main/java/com/robinlb99/legalserviceportal/LegalserviceportalApplication.java
+++ src/main/java/com/robinlb99/legalserviceportal/LegalserviceportalApplication.java
@@ -2,8 +2,10 @@ package com.robinlb99.legalserviceportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.robinlb99.legalserviceportal.domain")
 public class LegalserviceportalApplication {
 
 	public static void main(String[] args) {
