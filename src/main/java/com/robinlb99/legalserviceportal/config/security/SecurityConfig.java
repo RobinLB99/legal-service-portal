a1880b63340ee3e791dd4a900bbe9b8e81825a9c
Robin Lugo Boero <rjlb991102@hotmail.com>
2025-07-10 11:50:50 -0500

    chore: Eliminar importación no utilizada

--- src/main/java/com/robinlb99/legalserviceportal/config/security/SecurityConfig.java
+++ src/main/java/com/robinlb99/legalserviceportal/config/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.robinlb99.legalserviceportal.config.security;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
